e47f8f334bc27fdcddb8c8c21b5291df353612ea
OlegMaksimov <maksimovoleg19@gmail.com>
2019-05-10 18:55:40 +0300

    init

---
+++ src/sample/Controller.java
@@ -0,0 +1,15 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class Controller {
+
+    @FXML
+    private Button btn1;
+
+    @FXML
+    private void click() {
+        System.out.println("send hello");
+    }
+}
